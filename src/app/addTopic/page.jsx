db2526b444b6cde6f269b9801b2cbc44a4ebd410
Tushar Sharma <tttushar.789@gmail.com>
2024-04-29 13:15:39 +0530

    third commit

--- src/app/addTopic/page.jsx
+++ src/app/addTopic/page.jsx
@@ -1,12 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.prevenDefault();
+    if (!title) {
+      alert("Title is required");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title }),
+      });
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a topic");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <form className="flex flex-col gap-3">
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
       <input
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fits">
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fits"
+      >
         Add Topic
       </button>
     </form>
