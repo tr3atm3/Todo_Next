f75d4c49a1791aed6233d5fe31b0da57a369ba05
Tushar Sharma <tttushar.789@gmail.com>
2024-04-29 14:02:13 +0530

    fifth commit

--- src/app/editTopic/[id]/page.jsx
+++ src/app/editTopic/[id]/page.jsx
@@ -1,5 +1,21 @@
 import EditTopicForm from "@/components/EditTopicForm";
 
-export default function EdiTopic() {
-  return <EditTopicForm />;
+const getTopicById = async () => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch topic");
+    }
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+export default async function EdiTopic({ params }) {
+  const { id } = params;
+  const { topic } = await getTopicById(id);
+  const { title } = topic;
+  return <EditTopicForm id={id} title={title} />;
 }
--- src/components/EditTopicForm.jsx
+++ src/components/EditTopicForm.jsx
@@ -1,10 +1,40 @@
-export default function EditTopicForm() {
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function EditTopicForm({ id, title }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault;
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ newTitle }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to update topic");
+      }
+      router.refresh();
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <form className="flex flex-col gap-3">
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
       <input
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Title"
+        value={newTitle}
+        onChange={(e) => setNewTitle(e.target.value)}
       />
       <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
         Update Topic
